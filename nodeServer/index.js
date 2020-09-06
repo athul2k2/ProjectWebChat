ddbe6288229898fce373cc9cb7cfecb75f0f3dc4
athul krishna <athulkrishnapv6@gmail.com>
2020-09-06 15:52:39 +0400

    Node module and socket.io installed

---
+++ nodeServer/index.js
@@ -0,0 +1,10 @@
+//Node server which will handle socket io connetions
+const io = require('socket.io')(8000)
+
+const users = {};
+
+io.on('connection', socket=>{
+    socket.on('user-joined',name =>{
+        
+    })
+})
