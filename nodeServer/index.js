4beed27aab9e6242a41323af9a5c4b31ad3d1b41
athul krishna <athulkrishnapv6@gmail.com>
2020-09-07 14:32:38 +0400

    backend over

--- nodeServer/index.js
+++ nodeServer/index.js
@@ -5,13 +5,17 @@ const users = {};
 
 io.on('connection', socket=>{
     socket.on('new-user-joined',name =>{
-        console.log("Newuser" ,name)
+        console.log("New user" ,name)
         users[socket.id] = name;
         socket.broadcast.emit('user-joined' , name);
         
-    });
+    })
 
-    socket.on('send',message =>{
-        socket.broadcast.emit('receive',{message: message, name: user[socket.id]})
-    });
+    socket.on('send', message =>{
+        socket.broadcast.emit('receive' , {message: message, name:users [socket.id]})
+    })
+    socket.on('disconnect', message =>{
+        socket.broadcast.emit('left' , users[socket.id]);
+        delete users[socket.id];
+    })
 })
